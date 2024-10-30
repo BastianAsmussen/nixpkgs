5adbfad74ec1fef6eab69219b2c10025ee920eb0
Bastian Asmussen <bastian@asmussen.tech>
2024-10-30 14:56:13 +0100

    docker-desktop: 4.32.0 -> 4.34.0

--- pkgs/by-name/do/docker-desktop/package.nix
+++ pkgs/by-name/do/docker-desktop/package.nix
@@ -1,59 +1,84 @@
 {
+  lib,
   stdenv,
   fetchurl,
-  pkgs,
-  lib,
+  dpkg,
+  makeWrapper,
   buildFHSEnv,
-}: let
-  pname = "docker-desktop";
-  version = "4.34.0";
-  revision = 165256;
-
-  arch =
-    {
-      x86_64-linux = "amd64";
-    }
-    .${stdenv.hostPlatform.system};
-
-  src = fetchurl {
-    url = "https://desktop.docker.com/linux/main/${arch}/${toString revision}/${pname}-${arch}.deb?utm_source=nixpkgs";
-    sha256 = "sha256-qFepUUftBj7GgM2ZIiY8GjhAy16RRPjg2oW1pgbSYYk=";
-  };
-
-  runtimeDependencies = pkgs:
-    with pkgs; [
+  libseccomp,
+  libcap_ng,
+  alsa-lib,
+  nss,
+  gtk3,
+  mesa,
+  glib,
+  nspr,
+  dbus,
+  at-spi2-atk,
+  cups,
+  libdrm,
+  pango,
+  cairo,
+  xorg,
+  expat,
+}:
+let
+  fullPath = lib.makeLibraryPath (
+    [
+      stdenv.cc.cc
       libseccomp
       libcap_ng
       alsa-lib
       nss
       gtk3
       mesa
-    ];
+      glib
+      nspr
+      dbus
+      at-spi2-atk
+      cups.lib
+      libdrm
+      pango
+      cairo
+      expat
+    ]
+    ++ (with xorg; [
+      libX11
+      libXcomposite
+      libXdamage
+      libXext
+      libXfixes
+      libXrandr
+    ])
+  );
 
-  package = stdenv.mkDerivation rec {
-    inherit pname version revision src;
+  docker-desktop = stdenv.mkDerivation rec {
+    pname = "docker-desktop";
+    version = "4.34.0";
+    rev = "165256";
 
-    nativeBuildInputs = with pkgs; [
-      dpkg
-      autoPatchelfHook
-      makeWrapper
-      pkg-config
-    ];
+    src =
+      let
+        arch =
+          {
+            x86_64-linux = "amd64";
+          }
+          .${stdenv.hostPlatform.system};
+      in
+      fetchurl {
+        url = "https://desktop.docker.com/linux/main/${arch}/${rev}/${pname}-${arch}.deb";
+        sha256 = "sha256-qFepUUftBj7GgM2ZIiY8GjhAy16RRPjg2oW1pgbSYYk=";
+      };
 
-    buildInputs = runtimeDependencies pkgs;
+    nativeBuildInputs = [ makeWrapper ];
+    buildInputs = [ dpkg ];
 
-    sourceRoot = ".";
-    unpackCmd = "${pkgs.dpkg}/bin/dpkg-deb -x $src .";
-
-    dontConfigure = true;
-    dontBuild = true;
+    unpackPhase = "dpkg-deb -x ${src} .";
 
     installPhase = ''
-      runHook preInstall
+      mkdir -p $out/{bin,lib,share}
 
-      mkdir -p $out/bin
-
-      cp -R usr/{lib,share} opt $out/
+      cp -R usr/{bin,lib,share} opt $out/
 
       # Fix the path in the .desktop files.
       substituteInPlace \
@@ -62,35 +87,35 @@
 
       # Fix the path in the .service file.
       substituteInPlace \
-        $out/lib/systemd/user/${pname}.service \
+        $out/lib/systemd/user/docker-desktop.service \
         --replace-warn /opt/ $out/opt/
 
-      # Symlink the binary to bin/.
-      ln -s $out/opt/${pname}/Docker\ Desktop $out/bin/Docker\ Desktop
+      ln -s $out/opt/docker-desktop/Docker\ Desktop $out/bin/docker-desktop
 
       runHook postInstall
     '';
+
+    preFixup = ''
+      patchelf \
+        --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" \
+        --set-rpath "${fullPath}:$out/opt/docker-desktop" \
+        $out/opt/docker-desktop/Docker\ Desktop
+    '';
   };
 in
-  buildFHSEnv {
-    name = pname;
-    targetPkgs = pkgs:
-      (runtimeDependencies pkgs)
-      ++ [package];
+buildFHSEnv {
+  inherit (docker-desktop) pname version;
 
-    runScript = "docker-desktop";
-    extraInstallCommands = ''
-      mkdir -p $out/share/applications
+  targetPkgs = pkgs: [ docker-desktop ];
 
-      ln -s ${package}/share/applications/* $out/share/applications
-    '';
+  runScript = "docker-desktop";
 
-    meta = with lib; {
-      description = "Docker Desktop is an easy-to-install application that enables you to locally build and share containerized applications and microservices.";
-      homepage = "https://www.docker.com/products/docker-desktop";
-      license = licenses.unfree;
-      mainProgram = "docker-desktop";
-      platforms = platforms.linux;
-      maintainers = with maintainers; [BastianAsmussen];
-    };
-  }
+  meta = with lib; {
+    description = "Docker Desktop is an easy-to-install application that enables you to locally build and share containerized applications and microservices.";
+    homepage = "https://www.docker.com/products/docker-desktop";
+    license = licenses.unfree;
+    mainProgram = "docker-desktop";
+    platforms = platforms.linux;
+    maintainers = with maintainers; [ BastianAsmussen ];
+  };
+}
